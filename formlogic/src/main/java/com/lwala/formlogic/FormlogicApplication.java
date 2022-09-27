ef577dce59a325171186c79d65d0c325ae47acd1
Nelson Kimaiga <nelson.kimaiga@ubiqpay.io>
2022-09-27 16:09:20 +0300

    Initial commit, project set-up for both client and serverside

---
+++ formlogic/src/main/java/com/lwala/formlogic/FormlogicApplication.java
@@ -0,0 +1,13 @@
+package com.lwala.formlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormlogicApplication.class, args);
+	}
+
+}
---
+++ formlogic/src/test/java/com/lwala/formlogic/FormlogicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwala.formlogic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormlogicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
